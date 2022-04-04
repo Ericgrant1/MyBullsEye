646f2d5e94274d05a0db1eb6c033d798fe4c005f
Eric Grant <senatorericgrant@gmail.com>
2022-04-04 21:11:45 +0700

    feat: hook up button to an action alert

--- MyBullsEye/ViewController.swift
+++ MyBullsEye/ViewController.swift
@@ -13,5 +13,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(
+            title: "Hello, World",
+            message: "This is my app!",
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: "Awesome",
+            style: .default,
+            handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
