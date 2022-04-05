406ad119554ab397536ca296204cc49d0e1ef1af
Eric Grant <senatorericgrant@gmail.com>
2022-04-05 22:00:48 +0700

    refactor: get the difference

--- MyBullsEye/ViewController.swift
+++ MyBullsEye/ViewController.swift
@@ -20,19 +20,11 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showAlert() {
-        var difference: Int
-        
-        if currentValue > targetValue {
-            difference = currentValue - targetValue
-        } else if targetValue > currentValue {
-            difference = targetValue - currentValue
-        } else {
-            difference = 0
-        }
+        let difference = abs(targetValue - currentValue)
         
         let message = "The value of the slider is: \(currentValue)" +
                       "\nThe target value is: \(targetValue)" +
-                      "The difference is: \(difference)"
+                      "\nThe difference is: \(difference)"
         
         let alert = UIAlertController(
             title: "Hello, World",
